84ac26de301beebfc43ff9e61b05b3dcedc431f9
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-09-26 11:59:18 +0200

    Klasse Karte implementiert

--- src/main/java/ch/bbw/ms/Cluedo/model/Karte.java
+++ src/main/java/ch/bbw/ms/Cluedo/model/Karte.java
@@ -1,5 +1,28 @@
 package ch.bbw.ms.Cluedo.model;
 
-public class Karte {
-    
+public abstract class Karte {
+    private String name;
+    private String bildpfad;
+
+    public Karte(String name, String bildpfad) {
+        this.name = name;
+        this.bildpfad = bildpfad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBildpfad() {
+        return bildpfad;
+    }
+
+    public void setBildpfad(String bildpfad) {
+        this.bildpfad = bildpfad;
+    }
 }
+ 
