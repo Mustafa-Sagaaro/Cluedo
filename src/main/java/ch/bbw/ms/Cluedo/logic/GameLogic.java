406b29f7b165d8a89f524e9720d3e109d506e13e
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-09-26 12:23:18 +0200

    Klassen implementiert

--- src/main/java/ch/bbw/ms/Cluedo/logic/GameLogic.java
+++ src/main/java/ch/bbw/ms/Cluedo/logic/GameLogic.java
@@ -1,5 +1,40 @@
 package ch.bbw.ms.Cluedo.logic;
 
+import ch.bbw.ms.Cluedo.model.Crime;
+
 public class GameLogic {
-    
+
+    private int history = 0;
+
+    public boolean evaluateSuggestion(Crime suggestion, Crime secret,
+                                      int numberOfSuggestion, int maxNumberOfSuggestions) {
+        int matches = 0;
+
+        if(suggestion.getTaeter().equals(secret.getTaeter())) {
+            matches++;
+        }
+        if(suggestion.getTatwaffe().equals(secret.getTatwaffe())) {
+            matches++;
+        }
+        if(suggestion.getTatort().equals(secret.getTatort())) {
+            matches++;
+        }
+
+        history = matches;
+
+        if(matches == 3) {
+            return true;
+        }
+
+        if(numberOfSuggestion >= maxNumberOfSuggestions) {
+            history = -1;
+            return false;
+        }
+
+        return false;
+    }
+
+    public int getHistory() {
+        return history;
+    }
 }
--- src/main/java/ch/bbw/ms/Cluedo/model/Crime.java
+++ src/main/java/ch/bbw/ms/Cluedo/model/Crime.java
@@ -1,5 +1,39 @@
 package ch.bbw.ms.Cluedo.model;
 
 public class Crime {
-    
+    private String taeter;
+    private String tatwaffe;
+    private String tatort;
+
+    public Crime() {}
+
+    public Crime(String taeter, String tatwaffe, String tatort) {
+        this.taeter = taeter;
+        this.tatwaffe = tatwaffe;
+        this.tatort = tatort;
+    }
+
+    public String getTaeter() {
+        return taeter;
+    }
+
+    public void setTaeter(String taeter) {
+        this.taeter = taeter;
+    }
+
+    public String getTatwaffe() {
+        return tatwaffe;
+    }
+
+    public void setTatwaffe(String tatwaffe) {
+        this.tatwaffe = tatwaffe;
+    }
+
+    public String getTatort() {
+        return tatort;
+    }
+
+    public void setTatort(String tatort) {
+        this.tatort = tatort;
+    }
 }
