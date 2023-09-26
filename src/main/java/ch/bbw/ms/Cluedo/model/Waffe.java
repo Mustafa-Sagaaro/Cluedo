c1c4059acf326f9c59e791699ec3f94ec97fdfd7
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-09-26 12:02:56 +0200

    Klasse Waffe implementiert

--- src/main/java/ch/bbw/ms/Cluedo/model/Waffe.java
+++ src/main/java/ch/bbw/ms/Cluedo/model/Waffe.java
@@ -1,5 +1,9 @@
 package ch.bbw.ms.Cluedo.model;
 
-public class Waffe {
-    
+public class Waffe extends Karte {
+    public Waffe(String name, String bildpfad) {
+        super(name, bildpfad);
+    }
 }
+
+
