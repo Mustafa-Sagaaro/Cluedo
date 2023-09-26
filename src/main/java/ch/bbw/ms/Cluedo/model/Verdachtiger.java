16dbdfe7a732677934d2c0f47bdaaa548b492a29
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-09-26 12:00:59 +0200

    Klasse verdachtiger implementiert

---
+++ src/main/java/ch/bbw/ms/Cluedo/model/Verdachtiger.java
@@ -0,0 +1,10 @@
+package ch.bbw.ms.Cluedo.model;
+
+
+
+public class Verdachtiger extends Karte {
+    public Verdachtiger(String name, String bildpfad) {
+        super(name, bildpfad);
+    }
+}
+
