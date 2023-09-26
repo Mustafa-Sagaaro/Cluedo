b1a9d2ca8fa75450a29d71e92dd7e8b003c41afe
Mustafa <113579321+Mustafa-Sagaaro@users.noreply.github.com>
2023-09-26 12:02:07 +0200

    Klassse Vermutung update

--- src/main/java/ch/bbw/ms/Cluedo/model/VermutungErgebnis.java
+++ src/main/java/ch/bbw/ms/Cluedo/model/VermutungErgebnis.java
@@ -1,5 +1,39 @@
-package ch.bbw.ms.Cluedo.model;
+    package ch.bbw.ms.Cluedo.model;
 
-public class VermutungErgebnis {
-    
-}
+    public class VermutungErgebnis {
+        private boolean verdachtigerRichtig;
+        private boolean waffeRichtig;
+        private boolean zimmerRichtig;
+
+        public VermutungErgebnis() {}
+
+        public VermutungErgebnis(boolean verdachtigerRichtig, boolean waffeRichtig, boolean zimmerRichtig) {
+            this.verdachtigerRichtig = verdachtigerRichtig;
+            this.waffeRichtig = waffeRichtig;
+            this.zimmerRichtig = zimmerRichtig;
+        }
+
+        public boolean isVerdachtigerRichtig() {
+            return verdachtigerRichtig;
+        }
+
+        public void setVerdachtigerRichtig(boolean verdachtigerRichtig) {
+            this.verdachtigerRichtig = verdachtigerRichtig;
+        }
+
+        public boolean isWaffeRichtig() {
+            return waffeRichtig;
+        }
+
+        public void setWaffeRichtig(boolean waffeRichtig) {
+            this.waffeRichtig = waffeRichtig;
+        }
+
+        public boolean isZimmerRichtig() {
+            return zimmerRichtig;
+        }
+
+        public void setZimmerRichtig(boolean zimmerRichtig) {
+            this.zimmerRichtig = zimmerRichtig;
+        }
+    }
